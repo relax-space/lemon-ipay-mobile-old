1b63ac36961c8d3096d816e39aedf263c5b5fbe1
xiao.xinmiao <xiaoxm_001@163.com>
2017-12-05 11:09:13 +0800

    fastclick

--- src/components/KeyBoard.js
+++ src/components/KeyBoard.js
@@ -1,22 +1,23 @@
 import React from 'react';
 import { connect } from 'dva';
 import { routerRedux } from 'dva/router';
-import { List, InputItem } from 'antd-mobile';
+import { Modal, List, InputItem } from 'antd-mobile';
 import styles from './KeyBoard.css';
 
 function KeyBoard({ dispatch, list: dataSource, loading, total, page: current }) {
-//   function pageChangeHandler(page) {
-//     dispatch(routerRedux.push({
-//       pathname: '/keyBoard',
-//       query: { page },
-//     }));
-//   }  
+  //   function pageChangeHandler(page) {
+  //     dispatch(routerRedux.push({
+  //       pathname: '/keyBoard',
+  //       query: { page },
+  //     }));
+  //   }  
+
+
 
   return (
     <div className={styles.normal}>
-        <List>
-            <InputItem type="money" placeholder="￥" clear>金额</InputItem>
-        </List>
+              <InputItem type="money" placeholder="￥" clear>金额</InputItem>
+
     </div>
   );
 }

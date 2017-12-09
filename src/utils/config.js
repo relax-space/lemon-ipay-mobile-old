7f3969ed4de667b6ad389bdd72b9bc8598341531
xiao.xinmiao <xiaoxm_001@163.com>
2017-12-09 14:34:24 +0800

    staging

--- src/utils/config.js
+++ src/utils/config.js
@@ -18,18 +18,18 @@ export const backendAddr = (() => {
       notifyUrl: 'https://staging.p2shop.cn/ipay/v3/wx/notify',
     };
     case 'production': return {
-      wxPrepay: 'https://gateway.p2shop.cn/ipay/v3/wx/prepayeasy',
-      alPrepay: 'https://gateway.p2shop.cn/ipay/v3/al/prepay',
-      alNotifyUrl: 'https://gateway.p2shop.cn/ipay/v3/al/notify',
-      myUrl: 'https://ipay.p2shop.cn/#/pay',
-      notifyUrl: 'https://gateway.p2shop.cn/ipay/v3/wx/notify',
+      wxPrepay: 'https://staging.p2shop.cn/ipay/v3/wx/prepayeasy',
+      alPrepay: 'https://staging.p2shop.cn/ipay/v3/al/prepay',
+      alNotifyUrl: 'https://staging.p2shop.cn/ipay/v3/al/notify',
+      myUrl: 'https://ipay-staging.p2shop.cn/#/pay',
+      notifyUrl: 'https://staging.p2shop.cn/ipay/v3/wx/notify',
     };
     default: return {
-      wxPrepay: 'https://gateway.p2shop.cn/ipay/v3/wx/prepayeasy',
-      alPrepay: 'https://gateway.p2shop.cn/ipay/v3/al/prepay',
-      alNotifyUrl: 'https://gateway.p2shop.cn/ipay/v3/al/notify',
-      myUrl: 'https://ipay.p2shop.cn/#/pay',//'http://www.baidu.com',//
-      notifyUrl: 'https://gateway.p2shop.cn/ipay/v3/wx/notify',
+      wxPrepay: 'https://staging.p2shop.cn/ipay/v3/wx/prepayeasy',
+      alPrepay: 'https://staging.p2shop.cn/ipay/v3/al/prepay',
+      alNotifyUrl: 'https://staging.p2shop.cn/ipay/v3/al/notify',
+      myUrl: 'https://ipay-staging.p2shop.cn/#/pay',//'http://www.baidu.com',//
+      notifyUrl: 'https://staging.p2shop.cn/ipay/v3/wx/notify',
     };
   }
 })();
